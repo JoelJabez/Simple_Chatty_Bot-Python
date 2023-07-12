ce1e71e95df9b98ae135c58150dda6bc90d4931e
joel0135 <joeldavethu@gmail.com>
2023-07-12 22:06:37 +0800

    Jetbrains Academy, Simple Chatty Bot(Python) - Part 3/5

--- Simple Chatty Bot (Python)/task/bot/bot.py
+++ Simple Chatty Bot (Python)/task/bot/bot.py
@@ -1,7 +1,31 @@
-print("Hello! My name is Aid.")
-print("I was created in 2020.")
-print("Please, remind me your name.")
+def main():
+    print("Hello! My name is Aid.")
+    print("I was created in 2023.")
 
-name = input()
+    name = get_name("Please, remind me your name.")
+    print(f"What a great name you have, {name}!")
 
-print(f"What a great name you have, {name}!")
+    age = guess_age()
+    print(f"Your age is {age}; that's a good age to start programming")
+
+
+def get_name(text):
+    print(text)
+    return input()
+
+
+def guess_age():
+    print("Let me guess your age.")
+    print("Enter remainders of dividing your age by 3, 5 and 7.")
+
+    remainder_of_3 = int(input())
+    remainder_of_5 = int(input())
+    remainder_of_7 = int(input())
+
+    sum = (remainder_of_3 * 70) + (remainder_of_5 * 21) + (remainder_of_7 * 15)
+    actual_age = sum % 105
+
+    return actual_age
+
+
+main()
--- Simple Chatty Bot (Python)/task/test/tests.py
+++ Simple Chatty Bot (Python)/task/test/tests.py
@@ -4,33 +4,43 @@ from hstest.test_case import TestCase
 CheckResult.correct = lambda: CheckResult(True, '')
 CheckResult.wrong = lambda feedback: CheckResult(False, feedback)
 
-
 class ChattyBotTest(StageTest):
     def generate(self) -> List[TestCase]:
         return [
-            TestCase(stdin="John", attach="John"),
-            TestCase(stdin="Nick", attach="Nick")
+            TestCase(stdin="John\n1\n2\n1", attach=("John", 22)),
+            TestCase(stdin="Nick\n2\n0\n0", attach=("Nick", 35))
         ]
 
     def check(self, reply: str, clue: Any) -> CheckResult:
         lines = reply.strip().splitlines()
-        if len(lines) != 4:
+        if len(lines) != 7:
             return CheckResult.wrong(
-                "You should output 4 lines!\n" +
+                "You should output 7 lines!\n" +
                 f"Lines found: {len(lines)}"
                 f"Your output:\n"
                 f"{reply.strip()}"
             )
 
         line_with_name = lines[3].lower()
-        name = clue.lower()
+        name = clue[0].lower()
 
         if name not in line_with_name:
             return CheckResult.wrong(
-                "The name was " + clue + "\n" +
+                "The name was " + clue[0] + "\n" +
                 "But the 4-th line was:\n" +
                 "\"" + lines[3] + "\"\n\n" +
                 "4-th line should contain a name of the user"
+            )
+
+        line_with_age = lines[6].lower()
+        age = str(clue[1])
+
+        if age not in line_with_age:
+            return CheckResult.wrong(
+                "Can't find a correct age " +
+                "in the last line of output! " +
+                "Maybe you calculated the age wrong?\n\n" +
+                "Your last line: \n" + "\"" + lines[6] + "\""
             )
 
         return CheckResult.correct()
